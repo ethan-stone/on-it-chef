72a3a75fed05597953ddba91010f19a77328c26c
ethan-stone <ethan.stone9352@gmail.com>
2025-08-19 21:04:44 -0400

    add grafana user

--- infra/admin.ts
+++ infra/admin.ts
@@ -9,3 +9,60 @@ export const createAdminApiKeyFn = new sst.aws.Function("CreateAdminApiKeyFn", {
   },
   link: [secrets.mongoUrl],
 });
+
+const grafanaUser = new aws.iam.User("GrafanaUser", {
+  name: "grafana",
+});
+
+const grafanaRole = aws.iam.getPolicyDocument({
+  statements: [
+    {
+      effect: "Allow",
+      actions: [
+        "cloudwatch:DescribeAlarmsForMetric",
+        "cloudwatch:DescribeAlarmHistory",
+        "cloudwatch:DescribeAlarms",
+        "cloudwatch:ListMetrics",
+        "cloudwatch:GetMetricData",
+        "cloudwatch:GetInsightRuleReport",
+      ],
+      resources: ["*"],
+    },
+    {
+      effect: "Allow",
+      actions: ["pi:GetResourceMetrics"],
+      resources: ["*"],
+    },
+    {
+      effect: "Allow",
+      actions: [
+        "logs:DescribeLogGroups",
+        "logs:GetLogGroupFields",
+        "logs:StartQuery",
+        "logs:StopQuery",
+        "logs:GetQueryResults",
+        "logs:GetLogEvents",
+      ],
+      resources: ["*"],
+    },
+    {
+      effect: "Allow",
+      actions: [
+        "ec2:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+      ],
+      resources: ["*"],
+    },
+    {
+      effect: "Allow",
+      actions: ["tag:GetResources"],
+      resources: ["*"],
+    },
+  ],
+});
+
+new aws.iam.UserPolicy("GrafanaUserPolicy", {
+  user: grafanaUser.name,
+  policy: grafanaRole.then((role) => role.json),
+});
