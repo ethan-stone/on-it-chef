3fc018f09720d2f019abd8bf55b59d7cf6b93fff
ethan-stone <ethan.stone9352@gmail.com>
2025-08-23 19:39:43 -0400

    run change stream with node due to bun mongodb memory leak https://github.com/oven-sh/bun/issues/12117

--- infra/change-stream.ts
+++ infra/change-stream.ts
@@ -20,6 +20,7 @@ export const service = new sst.aws.Service("ChangeStreamService", {
   link: [secrets.mongoUrl, eventsTopic, memorySnapshotBucket],
   capacity: "spot", // Spot is okay for this use case. It's cheap and the change stream is resumable so it will pick up where it left off.
   scaling: {
+    min: 0,
     max: 1,
   },
 });
